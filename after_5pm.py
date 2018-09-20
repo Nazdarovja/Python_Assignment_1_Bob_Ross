7380495d3f1ee8aa0ff061b0a12ed200a2893386
awha86 <awha86@gmail.com>
2018-09-20 17:34:28 +0200

    upd to timestamp

--- after_5pm.py
+++ after_5pm.py
@@ -8,7 +8,7 @@ def after_5pm(file_name):
     json_list = convert(file_name)
     counter = 0
     for line in json_list:
-        if(int(line['timestamp'][11:13]) >= 17 or int(line['timestamp'][11:13]) < 3 ):  # between 17-03 (where dataset stops)
+        if(int(line['timestamp'][11:13]) >= 17 or int(line['timestamp'][11:13]) < 4 ):  # between 17-03 (where dataset stops)
             counter += 1
 
     return counter
