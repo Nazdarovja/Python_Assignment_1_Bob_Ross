d8675e95991a3bd8be98612bd4fc7f241041708a
awha86 <awha86@gmail.com>
2018-09-21 07:38:31 +0200

    upd after_5pm to be more pythonic; list comprehension syntax

--- after_5pm.py
+++ after_5pm.py
@@ -1,5 +1,12 @@
 def after_5pm(json_data):
+    """Takes list of json as input. Returns number chat messages after 17/5pm
+
+        Args: 
+            json_data: List of Dictionaries.
+
+        Returns: 
+            int, num of chat messages.
     """
-    Takes list of json as input. Returns number chat messages after 17/5pm.
-    """
+
+    # Dataset is limited to timestamps for <1 day, so possible to sort by hour >=17 or <4 o'clock. 
     return len([(int(line['timestamp'][11:13]) >= 17 or int(line['timestamp'][11:13]) < 4) for line in json_data])
