d9fd09cb7d1c63121982af14a51f7ae714644785
awha86 <awha86@gmail.com>
2018-09-21 07:33:05 +0200

    upd after_5pm to be more pythonic; list comprehension syntax

--- after_5pm.py
+++ after_5pm.py
@@ -2,10 +2,4 @@ def after_5pm(json_data):
     """
     Takes list of json as input. Returns number chat messages after 17/5pm.
     """
-    counter = 0
-    for line in json_data:
-        if(int(line['timestamp'][11:13]) >= 17 or int(line['timestamp'][11:13]) < 4 ):  # between 17-03 (where dataset stops)
-            counter += 1
-
-    return counter
-
+    return len([(int(line['timestamp'][11:13]) >= 17 or int(line['timestamp'][11:13]) < 4) for line in json_data])
